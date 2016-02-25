ec3f99bf3e4af37752e3207f746311db3034ea6c
Luke Tillman <luke.tillman@gmail.com>
2016-02-24 17:24:13 -0700

    Create a second webpack chunk for vendor dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 
 var Paths = {
   APP: path.resolve(__dirname, 'src/js'),
@@ -7,7 +8,14 @@ var Paths = {
 
 module.exports = {
   entry: {
-    app: Paths.APP
+    app: Paths.APP,
+    vendor: [
+      'bluebird', 'classnames', 'falcor', 'falcor-http-datasource', 'jsuri', 'load-script', 'lodash', 
+      'md5', 'moment', 'react', 'react-bootstrap', 'react-dom', 'react-dropzone', 'react-gemini-scrollbar', 
+      'react-redux', 'react-router', 'react-router-redux', 'redux', 'redux-actions', 'redux-form', 
+      'redux-logger', 'redux-promise-middleware', 'redux-thunk', 'reselect', 'socket.io-client', 'validate.js', 
+      'xhr'
+    ]
   },
   resolve: {
     extensions: [ '', '.js', '.jsx' ],
@@ -19,6 +27,12 @@ module.exports = {
     path: Paths.BUILD_OUTPUT,
     filename: 'killrvideo.js'
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({ 
+      name: 'vendor',
+      filename: 'vendor.js'
+    })
+  ],
   module: {
     loaders: [
       // Babel transpiler (see .babelrc file for presets)
