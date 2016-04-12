6ea3409fd76c5ff37be12527df6afe32fc48c83c
Luke Tillman <luke.tillman@gmail.com>
2016-04-12 14:00:34 -0600

    First crack at the recent videos route

--- src/routes/video-routes.js
+++ src/routes/video-routes.js
@@ -1,9 +1,12 @@
 import { ref as $ref, atom as $atom, error as $error } from 'falcor-json-graph';
 import Promise from 'bluebird';
 import { getClientAsync } from '../services/video-catalog';
-import { uuidToString, stringToUuid, timestampToDate } from '../utils/protobuf-conversions';
-import { getIndexesFromRanges, flattenPathValues } from '../utils/falcor-utils';
+import { uuidToString, stringToUuid, timestampToDate, dateToTimestamp } from '../utils/protobuf-conversions';
+import { getIndexesFromRanges, groupIndexesByPagingState, flattenPathValues } from '../utils/falcor-utils';
 import { logger } from '../utils/logging';
+
+// Constant used in routes below to indicate a list is empty
+const EMPTY_LIST_VALUE = 'NONE';
 
 // All routes supported by the video catalog service
 const routes = [
@@ -54,9 +57,129 @@ const routes = [
   },
   {
     // Get recent videos
-    route: 'recentVideos[{ranges:indexRanges}]["videoId", "name", "previewImageLocation", "addedDate" ]',
+    route: 'recentVideos',
     get(pathSet) {
+      // Reset paging state cached in session
+      let sess = this.req.session;
+      sess.recentVideosPagingStates = {
+        0: ''
+      };
       
+      // Figure out the latest video so we can return a reference to a list that's stable for paging
+      return getClientAsync()
+        .then(client => client.getLatestVideoPreviewsAsync({ pageSize: 1 }))
+        .then(response => {
+          let startingVideoToken = EMPTY_LIST_VALUE;
+          if (response.videoPreviews.length === 1) {
+            // Use video id and added date as token
+            startingVideoToken = `${uuidToString(response.videoPreviews[0].videoId)}_${timestampToDate(response.videoPreviews[0].addedDate).valueOf()}`;
+          }
+          return [
+            { path: [ 'recentVideos' ], value: $ref([ 'recentVideosList', startingVideoToken ]) }
+          ];
+        })
+        .catch(err => {
+          logger.log('error', 'Error while getting latest video preview', err);
+          return [
+            { path: [ 'recentVideos' ], value: $error() }
+          ];
+        });
+    }
+  },
+  {
+    // The recent videos list
+    route: 'recentVideosList[{keys:startingVideoTokens}][{ranges:indexRanges}]["videoId", "name", "previewImageLocation", "addedDate", "author"]',
+    get(pathSet) {
+      // This should only ever get called with a single starting video token, so do a sanity check
+      if (pathSet.startingVideoTokens.length != 1) {
+        logger.log('error', 'Got request for recent videos with %d starting video tokens', pathSet.startingVideoTokens.length);
+        return pathSet.startingVideoTokens.map(token => {
+          return { path: [ 'recentVideosList', token ], value: $error() };
+        });
+      }
+      
+      const startingVideoToken = pathSet.startingVideoTokens[0];
+      
+      // If list is empty, no need to go to service
+      if (startingVideoToken === EMPTY_LIST_VALUE) {
+        return getIndexesFromRanges(pathSet.indexRanges).map(idx => {
+          return { path: [ 'recentVideosList', startingVideoToken, idx ], value: $atom() };
+        });
+      }
+      
+      // Parse the token into video id and added date
+      const tokenParts = startingVideoToken.split('_');
+      const startingVideoId = stringToUuid(tokenParts[0]);
+      const startingAddedDate = dateToTimestamp(new Date(parseInt(tokenParts[1])));
+      
+      // The props to get for the videos
+      const videoProps = pathSet[3];
+      
+      // Get all indexes from the ranges and all available paging states saved in session
+      const allIndexes = getIndexesFromRanges(pathSet.indexRanges);
+      let pagingStates = this.req.session.recentVideosPagingStates;
+      
+      // Group the indexes by the paging state they can use for the query, then do queries      
+      const getPreviewsPromises = groupIndexesByPagingState(allIndexes, pagingStates).map(idxsAndPaging => {
+        const { startingIndex, pagingState, indexes, isLastAvailablePagingState } = idxsAndPaging;
+        
+        // Create a request object to call the service with
+        const getRequest = {
+          pageSize: indexes[indexes.length - 1],
+          startingAddedDate,
+          startingVideoId,
+          pagingState
+        };
+        
+        // Make the request
+        return getClientAsync()
+          .then(client => client.getLatestVideoPreviewsAsync(getRequest))
+          .tap(response => {
+            // Save the paging state to session if necessary
+            if (isLastAvailablePagingState && response.pagingState !== '') {
+              let nextStartingIndex = indexes[indexes.length - 1] + 1;
+              pagingStates[nextStartingIndex] = response.pagingState;
+            }
+          })
+          .then(response => {
+            const pathValues = indexes.map(idx => {
+              // Did we get a video preview at the index we're looking for?
+              const adjustedIdx = idx - startingIndex;
+              if (adjustedIdx >= response.videoPreviews.length) {
+                // Nope, just return an empty atom
+                return [
+                  { path: [ 'recentVideosList', startingVideoToken, idx ], value: $atom() }
+                ];
+              }
+              
+              // Yup, get the properties that were requested
+              const videoPreview = response.videoPreviews[adjustedIdx];
+              return videoProps.map(prop => {
+                const path = [ 'recentVideosList', startingVideoToken, idx, prop ];
+                switch (prop) {
+                  case 'videoId':
+                    return { path, value: uuidToString(videoPreview.videoId) };
+                  case 'addedDate':
+                    return { path, value: timestampToDate(videoPreview.addedDate) };
+                  case 'author':
+                    return { path, value: $ref([ 'usersById', uuidToString(videoPreview.userId) ]) };
+                  default:
+                    return { path, value: videoPreview[prop] };
+                }
+              });
+            });
+            
+            return flattenPathValues(pathValues);
+          })
+          .catch(err => {
+            logger.log('error', 'Error while getting latest videos', err);
+            return indexes.map(idx => {
+              return { path: [ 'recentVideosList', startingVideoToken, idx ], value: $error() }
+            });
+          });
+      });
+      
+      return Promise.all(getVideoPromises).then(flattenPathValues);
     }
   }
 ];
--- src/utils/falcor-utils.js
+++ src/utils/falcor-utils.js
@@ -1,8 +1,8 @@
 /**
- * Gets an array of indexes from an array of Falcor ranges
+ * Gets an array of indexes from an array of Falcor ranges.
  */
 export function getIndexesFromRanges(ranges) {
-  var idxs = [];
+  let idxs = [];
   for (let i = 0; i < ranges.length; i++) {
     let range = ranges[i];
     for (let j = range.from; j <= range.to; j++) {
@@ -10,6 +10,52 @@ export function getIndexesFromRanges(ranges) {
     }
   }
   return idxs;
+};
+
+/**
+ * Groups a list of indexes by available paging states and returns an array of objects.
+ */
+export function groupIndexesByPagingState(idxs, pagingStates) {
+  let results = [];
+  let psIdx = 0;
+  let newPage = true;
+  
+  // Make sure indexes are in order from low to high
+  const indexes = idxs.sort((a, b) => { return a - b });
+  
+  // Convert paging states object of key-value pairs into an array sorted by starting index
+  const psArray = Object.keys(pagingStates)
+    .map(idxStr => {
+      return { startingIndex: parseInt(idxStr), pagingState: pagingStates[idxStr] };
+    })
+    .sort((a, b) => { return a.startingIndex - b.startingIndex; });
+  
+  for (let indexesIdx = 0; indexesIdx < indexes.length; indexesIdx++) {
+    let curIdx = indexes[indexesIdx];
+    
+    // Find the index in paging states that's appropriate for the current index
+    while (psIdx < psArray.length - 1 && curIdx >= psArray[psIdx + 1].startingIndex) {
+      psIdx++;
+      newPage = true;
+    }
+    
+    // If this is a new page, add the result object to our results array
+    if (newPage === true) {
+      let { startingIndex, pagingState } = psArray[psIdx];
+      results.push({
+        startingIndex,
+        pagingState,
+        indexes: [],
+        isLastAvailablePagingState: (psIdx === psArray.length - 1)
+      });
+    }
+    
+    // Add the index to the results
+    results[results.length - 1].indexes.push(curIdx);
+    newPage = false;
+  }
+  
+  
 };
 
 /**
