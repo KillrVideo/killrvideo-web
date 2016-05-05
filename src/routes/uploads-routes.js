83a154e59334e450661795da05fe4d68d840c7bf
Luke Tillman <luke.tillman@gmail.com>
2016-05-05 11:05:48 -0600

    Error response for upload routes for the time being

--- src/routes/uploads-routes.js
+++ src/routes/uploads-routes.js
@@ -1,12 +1,15 @@
+import { toError } from './common/sentinels';
+
 // Routes served by the uploads service
 const routes = [
   {
     // Generate a URL where a video file can be uploaded to
     route: 'uploads.generateDestination',
     call(callPath, args) {
-      // TODO: Need to take another look at upload routes in the client and also provide a route and call it from
-      // the client to mark an upload as complete
-      throw new Error('Not implemented');
+      // TODO: What about marking uploads complete?
+      return [
+        { path: [ 'uploads', 'generateDestinationErrors' ], value: toError('Uploads are not currently supported') }
+      ];
     }
   }
 ];
