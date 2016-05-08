632bc3ce5c2b23a31dea24fb0a2a616b00fb3607
Luke Tillman <luke.tillman@gmail.com>
2016-05-08 14:09:45 -0600

    Move all client source files into subdirectory in prep for adding server source files

---
+++ src/client/js/actions/paged.js
@@ -0,0 +1,187 @@
+import { createAction } from 'redux-actions';
+import { model, falcorValues } from 'stores/falcor-model';
+import { isUndefined, get } from 'lodash';
+
+/**
+ * Paging action constants
+ */
+
+export const RESET = 'paged/RESET';
+export const REQUEST = 'paged/REQUEST';
+export const RECEIVE = 'paged/RECEIVE';
+export const CHANGE_PAGE = 'paged/CHANGE_PAGE';
+
+/**
+ * Private action creators
+ */
+
+function request(listId) {
+  return {
+    type: `paged/${listId}/REQUEST`,
+    payload: {
+      pagedType: REQUEST,
+      listId
+    }
+  };
+}
+
+function receive(listId, data, moreDataOnServer, queryModel) {
+  return {
+    type: `paged/${listId}/RECEIVE`,
+    payload: {
+      pagedType: RECEIVE,
+      listId,
+      data,
+      moreDataOnServer,
+      queryModel
+    }
+  };
+}
+
+function changePage(listId, currentPageIndex) {
+  return {
+    type: `paged/${listId}/CHANGE_PAGE`,
+    payload: {
+      pagedType: CHANGE_PAGE,
+      listId,
+      currentPageIndex
+    }
+  };
+}
+
+function reset(listId) {
+  return {
+    type: `paged/${listId}/RESET`,
+    payload: {
+      pagedType: RESET,
+      listId
+    }
+  };
+}
+
+/**
+ * Private action creators that are exported as part of the public API by the createPagedActions function.
+ */
+
+// Action to get the initial page of records in a list using falcor
+function getInitialPage(selectPagedState, queryRoot, queries) {
+  return (dispatch, getState) => {
+    let { _listId: listId, pagingConfig } = selectPagedState(getState());
+    
+    // Reset any existing state
+    dispatch(reset(listId));
+        
+    // Tell the UI we're loading
+    dispatch(request(listId));
+    
+    // Add paging range to queries
+    queries = queries.map(q => [ ...queryRoot, { from: 0, length: pagingConfig.recordsPerPage }, ...q ]);
+    
+    model.get(...queries).then(response => {
+      const responseRoot = get(response, [ 'json', ...queryRoot ]);
+
+      // Handle empty responses
+      if (isUndefined(responseRoot)) {
+        dispatch(receive(listId, [], false, null));
+        return;
+      }
+      
+      const queryModel = model.deref(responseRoot);
+      const data = falcorValues(responseRoot);
+      const moreDataOnServer = data.length === pagingConfig.recordsPerPage;
+      
+      dispatch(receive(listId, data, moreDataOnServer, queryModel));
+    });
+  };
+};
+
+// Go to the next page of records in the list
+function nextPageClick(selectPagedState, queries) {
+  return (dispatch, getState) => {
+    // Grab some of the current state
+    let { _listId: listId, _queryModel: queryModel, moreDataOnServer, data, currentPageIndex, pagingConfig } = selectPagedState(getState());
+    
+    const nextPageStartIdx = currentPageIndex + pagingConfig.incrementIndexPerPage;
+    const alreadyHaveSomeOfNextPage = data.length > nextPageStartIdx;
+    
+    // If no data on the server and no data on the next page available, just bail and do nothing
+    if (moreDataOnServer === false) {
+      if (alreadyHaveSomeOfNextPage === false) {
+        return;
+      }
+      
+      // No data on server, but some records to show so go to the next page
+      dispatch(changePage(listId, nextPageStartIdx));
+      return;
+    }
+    
+    // Do we have the full next page to show without going to the server?
+    const nextPageEndIdx = nextPageStartIdx + pagingConfig.recordsPerPage - 1;
+    if (nextPageEndIdx < data.length) {
+      dispatch(changePage(listId, nextPageStartIdx));
+      return;
+    }
+    
+    // There are more pages available on the server and we need them, so go get them then go to the next page
+    dispatch(request(listId));
+    
+    queries = queries.map(q => [ { from: data.length, length: pagingConfig.recordsPerRequest }, ...q ]);
+    queryModel.get(...queries).then(response => {
+      const newData = isUndefined(response) ? [] : falcorValues(response.json);
+      const moreDataAvailable = newData.length === pagingConfig.recordsPerRequest;
+      dispatch(receive(listId, newData, moreDataAvailable));
+      
+      // If we got an empty page of data, make sure we actually have data on the next page to show
+      if (newData.length === 0 && alreadyHaveSomeOfNextPage === false) {
+        return;
+      }
+      
+      dispatch(changePage(listId, nextPageStartIdx));
+    });
+  };
+};
+
+// Go to the previous page of records in the list
+function previousPageClick(selectPagedState) {
+  return (dispatch, getState) => {
+    let { _listId: listId, currentPageIndex, pagingConfig } = selectPagedState(getState());
+    
+    if (currentPageIndex === 0) return;
+    dispatch(changePage(listId, currentPageIndex - pagingConfig.incrementIndexPerPage));
+  };
+};
+
+// Reset state when unloaded
+function unload(selectPagedState) {
+  return (dispatch, getState) => {
+    let { _listId: listId } = selectPagedState(getState());
+    dispatch(reset(listId));
+  };
+}
+
+
+/**
+ * Function for creating an object with the actions bound to a given list/paging config. The listId
+ * should uniquely identify a list. The selectPagedState function should take one parameter (the current 
+ * state from redux) and return the part of the state tree where the list's reducer is mounted.
+ */
+export function createPagedActions(selectPagedState) {
+  // Return an object with the public API's actions  
+  return {
+    getInitialPage(queryRoot, queries) {
+      return getInitialPage(selectPagedState, queryRoot, queries);
+    },
+    
+    nextPageClick(queries) {
+      return nextPageClick(selectPagedState, queries);
+    },
+    
+    previousPageClick() {
+      return previousPageClick(selectPagedState);
+    },
+    
+    unload() {
+      return unload(selectPagedState);
+    }
+  };
+};
---
+++ src/client/js/views/account/info/index.jsx
@@ -0,0 +1,112 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Row, Col, Button } from 'react-bootstrap';
+import { isUndefined } from 'lodash';
+import { routeActions } from 'react-router-redux';
+
+import { load, unload, showMoreComments, videosNextPage, videosPreviousPage } from 'actions/account-info';
+import Icon from 'components/shared/icon';
+import UserProfile from './user-profile';
+import UserComments from './user-comments';
+import UserVideos from './user-videos';
+
+// Styles needed by the view
+require('account-info.css');
+
+class AccountInfo extends Component {
+  componentDidMount() {
+    this.props.load(this.props.userId, AccountInfo.queries.user(), AccountInfo.queries.comments(), AccountInfo.queries.videos());
+  }
+  
+  componentDidUpdate(prevProps) {
+    // If the user id changes, we need to reload
+    if (prevProps.userId !== this.props.userId) {
+      this.props.load(this.props.userId, AccountInfo.queries.user(), AccountInfo.queries.comments(), AccountInfo.queries.videos());
+    }
+  }
+  
+  componentWillUnmount() {
+    this.props.unload();
+  }
+  
+  gotoAddVideo() {
+    this.props.push('/videos/add');
+  }
+  
+  render() {
+    const { userId, accountInfo: { user, comments, videos }, showMoreComments, videosNextPage, videosPreviousPage } = this.props;
+    
+    return (
+      <div className="body-content container">
+        <Row>
+          <Col md={5}>
+            <h3>
+              <Icon name="info-circle" /> Profile
+            </h3>
+            
+            <UserProfile user={user} />
+            
+            <h3>
+              <Icon name="comments" /> Latest Comments
+            </h3>
+            
+            <UserComments comments={comments} showMoreComments={showMoreComments} />
+          </Col>
+          <Col md={7}>
+            <h3>
+              <Icon name="video-camera" /> {isUndefined(userId) ? 'My' : undefined} Videos
+            </h3>
+            
+            <p className={isUndefined(userId) ? undefined : 'hidden'}>
+              <Button bsStyle="success" onClick={() => this.gotoAddVideo()}>
+                <Icon name="plus-circle" /> Add a Video
+              </Button>
+            </p>
+            
+            <UserVideos videos={videos} nextPage={videosNextPage} previousPage={videosPreviousPage} />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+// Prop validation
+AccountInfo.propTypes = {
+  // From router
+  userId: PropTypes.string,
+  // From state
+  accountInfo: PropTypes.object.isRequired,
+  // Actions
+  load: PropTypes.func.isRequired, 
+  unload: PropTypes.func.isRequired, 
+  loadMoreComments: PropTypes.func.isRequired, 
+  videosNextPage: PropTypes.func.isRequired, 
+  videosPreviousPage: PropTypes.func.isRequired,
+  push: PropTypes.func.isRequired
+};
+
+// Falcor queries
+AccountInfo.queries = {
+  user: UserProfile.queries.user,
+  comments: UserComments.queries.comments,
+  videos: UserVideos.queries.videos
+};
+
+// Map redux state to component props
+function mapStateToProps(state, ownProps) {
+  return {
+    userId: ownProps.params.userId,
+    accountInfo: state.accountInfo
+  };
+}
+
+// Connect to redux and export
+export default connect(mapStateToProps, {
+  load, 
+  unload, 
+  showMoreComments, 
+  videosNextPage, 
+  videosPreviousPage, 
+  push: routeActions.push 
+})(AccountInfo);
