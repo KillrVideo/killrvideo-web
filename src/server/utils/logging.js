bffcb527bbd9d45f57e1b2534377f61402e9048a
Luke Tillman <luke.tillman@gmail.com>
2016-05-09 09:57:03 -0600

    Make logging configurable and allow logging to a file

--- src/server/utils/logging.js
+++ src/server/utils/logging.js
@@ -1,12 +1,34 @@
-import { Logger, transports } from 'winston';
+import { Logger, transports as CoreTransports } from 'winston';
+import config from 'config';
+import { join } from 'path';
+import mkdirp from 'mkdirp';
+
+// Figure out what transports to use from config
+let transports = [];
+
+if (config.get('logging.console.enabled') === true) {
+  transports.push(new CoreTransports.Console());
+}
+
+if (config.get('logging.file.enabled') === true) {
+  // Ensure the path to the logs exists
+  let p = join(process.cwd(), config.get('logging.file.path'));
+  mkdirp.sync(p);
+  
+  // Add file (stream) transport
+  transports.push(new CoreTransports.File({
+    filename: join(p, config.get('logging.file.name')),
+    maxsize: 1000000,
+    maxFiles: 10,
+    tailable: true,
+    json: false
+  }));
+}
+
 
 /**
  * The default logger for the application.
  */
-export const logger = new Logger({
-  transports: [
-    new transports.Console()
-  ]
-});
+export const logger = new Logger({ transports });
 
 export default logger;
