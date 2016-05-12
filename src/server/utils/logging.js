fbc05e8783b9fa68511e61f119651bf154d0b3bd
Luke Tillman <luke.tillman@gmail.com>
2016-05-11 18:31:00 -0600

    Just go back to console only logging since that's good enough for docker logs support

--- src/server/utils/logging.js
+++ src/server/utils/logging.js
@@ -1,30 +1,8 @@
 import { Logger, transports as CoreTransports } from 'winston';
-import config from 'config';
-import { join } from 'path';
-import mkdirp from 'mkdirp';
 
-// Figure out what transports to use from config
+// Just log to the consoleby default
 let transports = [];
-
-if (config.get('logging.console.enabled') === true) {
-  transports.push(new CoreTransports.Console());
-}
-
-if (config.get('logging.file.enabled') === true) {
-  // Ensure the path to the logs exists
-  let p = join(process.cwd(), config.get('logging.file.path'));
-  mkdirp.sync(p);
-  
-  // Add file (stream) transport
-  transports.push(new CoreTransports.File({
-    filename: join(p, config.get('logging.file.name')),
-    maxsize: 1000000,
-    maxFiles: 10,
-    tailable: true,
-    json: false
-  }));
-}
-
+transports.push(new CoreTransports.Console());
 
 /**
  * The default logger for the application.
