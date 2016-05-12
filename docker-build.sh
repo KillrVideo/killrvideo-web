8bc16b12d444da3518c5bbbe78ed1791c442faa0
Luke Tillman <luke.tillman@gmail.com>
2016-05-12 15:45:02 -0600

    Helper script for doing docker build with correct tags

---
+++ docker-build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Get the package version from the package.json then build with docker build
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+docker build -t luketillman/killrvideo-web:latest -t luketillman/killrvideo-web:$PACKAGE_VERSION .
