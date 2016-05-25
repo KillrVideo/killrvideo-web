4d6807ab5852ca5d9aefb087995ce6b2348bde73
Luke Tillman <luke.tillman@gmail.com>
2016-05-25 08:08:03 -0600

    Bug fixes/improvements to service lookup

--- src/server/utils/lookup-service.js
+++ src/server/utils/lookup-service.js
@@ -1,19 +1,20 @@
-import dns from 'dns';
+import { resolve, NODATA, NOTFOUND } from 'dns';
+import { format } from 'util';
 import config from 'config';
 import Promise from 'bluebird';
 import { logger } from './logging';
 import { ExtendableError } from './extendable-error';
 
-const resolveAsync = Promise.promisify(dns.resolve);
-const hosts = config.get('hosts');
+const resolveAsync = Promise.promisify(resolve);
+const configLookup = config.get('serviceLookup');
 
 function handleResolveErrors(hostname, recordType, defaultReturnValue) {
-  const errMsg = `Could not find ${recordType} record for ${hostname}`;
+  const notFoundMsg = `Could not find ${recordType} record for ${hostname}`;
   
   return function resolveErrorHandler(err) {
     // If not found, just return an empty array
-    if (err.code === dns.NODATA || err.code === dns.NOTFOUND) {
-      logger.log('verbose', errMsg);
+    if (err.code === NODATA || err.code === NOTFOUND) {
+      logger.log('verbose', notFoundMsg);
       return defaultReturnValue;
     }
     
@@ -34,35 +35,36 @@ export class UnableToResolveServiceError extends ExtendableError {
 };
 
 /**
- * Looks up a service with a given hostname. Returns an array of strings in the format of 'ip:port'.
+ * Looks up a service with a given name. Returns an array of strings in the format of 'ip:port'.
  */
 export function lookupServiceAsync(serviceName, defaultPort) {
   logger.log('verbose', `Looking up service ${serviceName}`);
   
   // Allow lookup to be overridden in config file
-  if (hosts.has(serviceName)) {
-    let resolved = hosts.get(serviceName);
+  if (configLookup.has(serviceName)) {
+    let resolved = configLookup.get(serviceName);
     if (Array.isArray(resolved) === false) {
       resolved = [ resolved ];
     }
-    logger.log('verbose', `Found service ${serviceName} at %j in hosts config`, resolved);
+    logger.log('verbose', `Found service ${serviceName} at ${resolved} in serviceLookup config`);
     return Promise.resolve(resolved);
   }
   
-  // Construct a hostname from the service name and the environment
-  let env = config.util.getEnv('NODE_ENV');
-  let hostname = `${serviceName}.${env}.killrvideo`;
+  // Construct a hostname from the service name
+  let hostname = `${serviceName}.service.killrvideo`;
   
   logger.log('verbose', `Attempting to resolve ${hostname}`);
   
   // Lookup SRV records
   let srvPromise = resolveAsync(hostname, 'SRV')
-    .catch(err => handleResolveErrors(hostname, 'SRV', [ { name: hostname, port: defaultPort } ]));
+    .catch(handleResolveErrors(hostname, 'SRV', [ { name: hostname, port: defaultPort } ]))
+    .tap(records => { logger.log('verbose', `Resolved SRV records for ${hostname} to ${JSON.stringify(records)}`); });
     
   // Use SRV records to resolve A records
   let aPromise = srvPromise.map(srvAddress => {
     return resolveAsync(srvAddress.name, 'A')
-      .catch(err => handleResolveErrors(srvAddress.name, 'A', []))
+      .catch(handleResolveErrors(srvAddress.name, 'A', []))
+      .tap(addresses => { logger.log('verbose', `Resolved A records for ${srvAddress.name} to ${JSON.stringify(addresses)}`); })
       .reflect();
   });
   
@@ -95,7 +97,13 @@ export function lookupServiceAsync(serviceName, defaultPort) {
     
     throw new UnableToResolveServiceError(serviceName, errors);
   })
+  .catch(err => {
+    if (err instanceof UnableToResolveServiceError === false) {
+      logger.log('error', `Error while resolving ${hostname}`, err);
+    }
+    throw err;
+  })
   .tap(hosts => {
-    logger.log('verbose', `Found service ${serviceName} at %j in DNS`, hosts);
+    logger.log('verbose', `Found service ${serviceName} at ${JSON.stringify(hosts)} in DNS`);
   });
 };
