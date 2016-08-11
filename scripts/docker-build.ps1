08ef09bfdbbfac17282d6af23d130f334a5e3252
Luke Tillman <luke.tillman@gmail.com>
2016-08-11 16:33:26 -0600

    Switch to killrvideo org for docker images

--- scripts/docker-build.ps1
+++ scripts/docker-build.ps1
@@ -1,3 +1,3 @@
 # Get the package version from the package.json then build with docker build
 $PACKAGE_VERSION = (node -p -e "require('./package.json').version")
-docker build -t luketillman/killrvideo-web:latest -t "luketillman/killrvideo-web:$PACKAGE_VERSION" .
+docker build -t "killrvideo/killrvideo-web:$PACKAGE_VERSION" .
