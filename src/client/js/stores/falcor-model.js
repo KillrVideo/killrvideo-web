4d5b7773d7e0447f2042a0d4c17c0d618b735840
Luke Tillman <luke.tillman@gmail.com>
2016-09-13 13:02:04 -0600

    Go back to old way of wrapping falcor model methods
    - Trying to subclass doesn't work because batch() doesn't return our class, it returns the original Model class from falcor

--- src/client/js/stores/falcor-model.js
+++ src/client/js/stores/falcor-model.js
@@ -2,31 +2,25 @@ import falcor from 'falcor';
 import HttpDataSource from 'falcor-http-datasource';
 import { Promise } from 'lib/promise';
 
-class FalcorModel extends falcor.Model {
-  constructor() {
-    super({
-      // Do queries via HTTP to model.json with a 60s timeout to allow devs time to step through code
-      source: new HttpDataSource('/model.json', { timeout: 60000 })
-    });
-  }
-
-  get(...args) {
-    return Promise.resolve(super.get(...args));
-  }
-
-  set(...args) {
-    return Promise.resolve(super.set(...args));
-  }
-
-  call(...args) {
-    return Promise.resolve(super.call(...args));
-  }
+// Wrap the given function with Promise.resolve 
+function wrapWithPromise(fn) {
+  return function withPromise(...args) {
+    return Promise.resolve(fn.apply(this, args));
+  };
 }
 
+// Do queries via HTTP to /model.json with a 60s timeout to allow devs time to step through code
+const model = new falcor.Model({
+  source: new HttpDataSource('/model.json', { timeout: 60000 })
+}).batch();
+
+// Wrap model async methods so they return Promises
+model.call = wrapWithPromise(model.call);
+model.get = wrapWithPromise(model.get);
+model.set = wrapWithPromise(model.set);
+
 // Export singleton instance of the Falcor Model
-const model = new FalcorModel().batch();
 export { model };
-export default model;
 
 /**
  * Helper function like lodash's values function but users Falcor.keys to enumerate the object's
