6e2999efe9be5c4e234f1135ea18c228071d6cf7
Luke Tillman <luke.tillman@gmail.com>
2016-09-13 10:40:48 -0600

    Update 'What is This?' section text/links. Fixes #2

--- src/client/js/views/layout/what-is-this.jsx
+++ src/client/js/views/layout/what-is-this.jsx
@@ -13,14 +13,13 @@ class WhatIsThis extends Component {
               <div className="col-sm-4">
                 <h4><em>What</em> is KillrVideo?</h4>
                 <p>
-                  KillrVideo is a sample web application developed in C# and  
-                  uses <a href="http://www.datastax.com/products/datastax-enterprise" target="_blank">DataStax Enterprise</a> running 
-                  on Microsoft Azure as the database platform. The site demonstrates a number of DataStax Enterprise components in action
-                  including <a href="http://www.datastax.com/products/datastax-enterprise-production-certified-cassandra" target="_blank">Apache Cassandra</a>  
-                  , <a href="http://www.datastax.com/products/datastax-enterprise-analytics" target="_blank">DSE Analytics</a>  
-                  , and <a href="http://www.datastax.com/products/datastax-enterprise-search" target="_blank">DSE Search</a>. The application's  
-                  code, data models, data and more are freely available on GitHub.
+                  KillrVideo is a reference web application for users looking to learn about using their programming language
+                  of choice with <a href="http://cassandra.apache.org" target="_blank">Apache Cassandra</a> and <a href="http://www.datastax.com/products/datastax-enterprise" target="_blank">DataStax Enterprise</a>.
                 </p>
+                <br />
+                <Button bsStyle="success" href="https://killrvideo.github.io/docs/" target="_blank">
+                  <Icon name="book" /> Documentation
+                </Button>
                 <hr className="visible-xs-block"/>
               </div>
               
@@ -28,10 +27,10 @@ class WhatIsThis extends Component {
                 <h4><em>Get</em> the code!</h4>
                 <p>
                   All the code, including the CQL used to create the schema for the site is available on GitHub. Pull requests
-                  for any bugs, improvements, etc. are accepted (and greatly appreciated).
+                  for any bugs, improvements, etc. are greatly appreciated.
                 </p>
                 <br/>
-                <Button bsStyle="success" href="https://github.com/luketillman/killrvideo-csharp" target="_blank">
+                <Button bsStyle="success" href="https://killrvideo.github.io/getting-started/" target="_blank">
                   <Icon name="github"/> Get the code
                 </Button>
                 <hr className="visible-xs-block"/>
@@ -43,11 +42,9 @@ class WhatIsThis extends Component {
                   There are many free resources available to help you learn about DataStax Enterprise and Cassandra, including:
                 </p>
                 <ul>
-                  <li><a href="https://academy.datastax.com/" target="_blank">DataStax Academy</a> (free online training for Cassandra and DSE)</li>
+                  <li><a href="https://academy.datastax.com/" target="_blank">DataStax Academy</a> (free online training)</li>
                   <li><a href="http://docs.datastax.com/en/index.html" target="_blank">DataStax Documentation</a></li>
-                  <li><a href="http://docs.datastax.com/en/getting_started/doc/getting_started/intro.html" target="_blank">Getting Started</a> with DataStax Enterprise and Cassandra documentation</li>
-                  <li><a href="http://www.datastax.com/" target="_blank">DataStax website</a> (software downloads and more)</li>
-                  <li><a href="http://planetcassandra.org/" target="_blank">Planet Cassandra</a> website</li>
+                  <li><a href="http://cassandra.apache.org/doc/latest/" target="_blank">Apache Cassandra Documentation</a></li>
                 </ul>
   
                 { /* On mobile, add a link to hide the what is this UI */ }
