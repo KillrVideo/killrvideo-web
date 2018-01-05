f23b70060241e425d361b5b337e4e7b3a065823a
Jeffrey Carpenter <jeffreyscarpenter@cox.net>
2018-01-05 15:53:35 -0700

    email no longer required for docker publish

--- scripts/docker-publish.sh
+++ scripts/docker-publish.sh
@@ -3,7 +3,6 @@
 # This script assumes the following environment variables are present
 #
 #   DOCKER_USER=username
-#   DOCKER_EMAIL=some@email.com
 #   DOCKER_PASS=somesecretpassword
 DOCKER_IMAGE=killrvideo/killrvideo-web
 
@@ -20,13 +19,13 @@ if [ -z "$CURRENT_TAG" ]; then
 fi
 
 # Make sure we have a user/pass
-if [[ -z "$DOCKER_USER" ]] || [[ -z "$DOCKER_EMAIL" ]] || [[ -z "$DOCKER_PASS" ]]; then
-  echo "DOCKER_USER or DOCKER_EMAIL or DOCKER_PASS not set"
+if [[ -z "$DOCKER_USER" ]] || [[ -z "$DOCKER_PASS" ]]; then
+  echo "DOCKER_USER or DOCKER_PASS not set"
   exit 1
 fi
 
 echo "Publishing $DOCKER_IMAGE for git tag $CURRENT_TAG"
 
 # Login to Docker and push the image which should have been built
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push $DOCKER_IMAGE
